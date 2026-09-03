90385690a5c5198d3c18e7a926bd23f6d5c15f1e
Aiden Ford <aford277@hotmail.com>
2026-09-02 20:09:17 -0400

    Added navigation bar button

---
+++ lib/screens/my_listings_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyListingsPage extends StatelessWidget {
+  const MyListingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Under Construction',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
